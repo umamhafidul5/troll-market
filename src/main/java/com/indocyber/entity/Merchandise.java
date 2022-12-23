c18361daf2a5855effe95233abaf8fad3ce2342b
Nyihui <aphroditeradhika@tutanota.com>
2022-12-23 16:06:55 +0700

    Push

---
+++ src/main/java/com/indocyber/entity/Merchandise.java
@@ -0,0 +1,100 @@
+package com.indocyber.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Merchandise")
+public class Merchandise {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "SellerUsername")
+    private Account seller;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Category")
+    private String category;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Price")
+    private BigDecimal price;
+
+    @Column(name = "IsDiscontinue")
+    private boolean isDiscontinue;
+
+    public Merchandise() {}
+
+    public Merchandise(Account seller, String name, String category, String description, BigDecimal price, boolean isDiscontinue) {
+        this.seller = seller;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.isDiscontinue = isDiscontinue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Account getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Account seller) {
+        this.seller = seller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isDiscontinue() {
+        return isDiscontinue;
+    }
+
+    public void setDiscontinue(boolean discontinue) {
+        isDiscontinue = discontinue;
+    }
+}
