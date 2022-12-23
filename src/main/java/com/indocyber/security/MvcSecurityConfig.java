878d418ab4a53d7f5b305331822d7ab7659eb5b4
darismarestu <darismarestuhidayatt@gmail.com>
2022-12-23 17:12:21 +0700

    security
    menambahkan mvc security, user details, authenticationprovider

---
+++ src/main/java/com/indocyber/security/MvcSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.indocyber.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MvcSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                .antMatchers("/css/**", "/account/**").permitAll()
+//                .antMatchers("/account/**", "/api/**").permitAll()
+//                .antMatchers("/**").hasAuthority("Super_Admin")
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/my-login/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser").permitAll()
+//                .successHandler(loginHandlerRedirect) // multiple landing
+                .and()
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/account/access-denied");
+        return httpSecurity.build();
+    };
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
