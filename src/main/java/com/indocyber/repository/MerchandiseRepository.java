e1177fc306bbc5e27df7225f1e86a88466f2b45c
umamhafidul5 <umamhafidul5@gmail.com>
2022-12-26 16:02:52 +0700

    menambahkan halaman menu shop

--- src/main/java/com/indocyber/repository/MerchandiseRepository.java
+++ src/main/java/com/indocyber/repository/MerchandiseRepository.java
@@ -3,5 +3,9 @@ package com.indocyber.repository;
 import com.indocyber.entity.Merchandise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MerchandiseRepository extends JpaRepository<Merchandise, Integer> {
+
+    List<Merchandise> getMerchandisesByNameOrCategoryOrDescription(String name, String category, String description);
 }
--- src/main/java/com/indocyber/service/MerchandiseServiceImpl.java
+++ src/main/java/com/indocyber/service/MerchandiseServiceImpl.java
@@ -1,10 +1,25 @@
 package com.indocyber.service;
 
+import com.indocyber.entity.Merchandise;
+import com.indocyber.repository.MerchandiseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class MerchandiseServiceImpl implements MerchandiseService {
+    private final MerchandiseRepository merchandiseRepository;
+
+    @Autowired
+    public MerchandiseServiceImpl(MerchandiseRepository merchandiseRepository) {
+        this.merchandiseRepository = merchandiseRepository;
+    }
+
+    @Override
+    public List<Merchandise> getAllMerchandiseBy(String name, String category, String description) {
+        return merchandiseRepository.getMerchandisesByNameOrCategoryOrDescription(name, category, description);
+    }
 }
