5806eed60e87d7c1949457d802ed730639429135
darismarestu <darismarestuhidayatt@gmail.com>
2022-12-28 10:21:34 +0700

    fix minor

--- src/main/java/com/indocyber/controller/mvc/MerchandiseController.java
+++ src/main/java/com/indocyber/controller/mvc/MerchandiseController.java
@@ -59,11 +59,9 @@ public class MerchandiseController {
                               BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
-<<<<<<< HEAD
             bindingResult.getAllErrors().forEach(System.out::println);
             model.addAttribute("account", accountService.getAccount());
-=======
->>>>>>> cfa365c615add0d49928bccfe5d001b1e562c9df
+
             return "add-merchandise-page";
         }
 
